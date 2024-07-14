30c185128a0a90a68fd63f04acf59ff1e05eb8b4
MisanToghanro <tmisan657@gmail.com>
2024-07-14 21:54:46 +0100

    add quiz.js

--- quiz.js
+++ quiz.js
@@ -3,7 +3,7 @@ let correctAnswer = "4";
 let userAnswer = document.querySelector("input[name='quiz']:checked").value;
 
 
-if(userAnswer === correctAnswer){
+if (userAnswer === correctAnswer){
     document.getElementById("feedback").textContent="Correct! well done.";
 } else{
     document.getElementById("feedback").textContent="That's incorrect. Try again!";
